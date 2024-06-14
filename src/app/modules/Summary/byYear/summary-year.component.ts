40532e7e57f27ac75367382e54c115fe1fc0fae5
Ariel Brenman <abrenman@mvfactory.com>
2024-06-14 09:12:32 -0300

    sort year

--- src/app/modules/Summary/byYear/summary-year.component.ts
+++ src/app/modules/Summary/byYear/summary-year.component.ts
@@ -78,7 +78,7 @@ export default class SummaryByYearComponent implements OnInit {
           }
           this.list.push(newItem);
          });
-        this.list = this.list.sort((a,b) => b.date.localeCompare(a.date));
+         const list = res.sort((a, b) => new Date(b.date).getDate() - new Date(a.date).getDate());
         this.dataSource.data =  this.list;
       } 
     });
